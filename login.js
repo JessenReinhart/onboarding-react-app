59d4149a7c454baa792eafb681af26905847189d
JessenReinhart <jessen@codeindonesia.com>
2019-11-13 18:07:45 +0700

    1 file modified

--- login.js
+++ login.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './style.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./style.css";
 
-class Login extends Component {
-  constructor() {
-    super();
+const Login = () => {
+  
+  const login = () => {
+    alert("Logged in!")
   }
 
-  render() {
-    return (
-      <div className="login">
-        <h3>Login</h3>
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <button className="main-button">Login</button>
-        <Link to="/register" className="secondary-button">Register</Link>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="login">
+      <h3>Login</h3>
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+      <button className="main-button" onClick={login}>
+        Login
+      </button>
+      <Link to="/register" className="secondary-button">
+        Register
+      </Link>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
